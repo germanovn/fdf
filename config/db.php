e2c62cf2773ae068b1ae83a9108589af0f82889c
Nick <germanovbiz@gmail.com>
2019-06-05 10:53:25 +0500

    Untest

--- config/db.php
+++ config/db.php
@@ -7,7 +7,6 @@ return [
     'password' => '',
     'charset' => 'utf8',
 
-
     // Schema cache options (for production environment)
     //'enableSchemaCache' => true,
     //'schemaCacheDuration' => 60,
