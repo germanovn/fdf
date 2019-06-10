c9cf5b419739abdfea5108c5fd2427a3f653920f
Nick <germanovbiz@gmail.com>
2019-06-10 13:15:34 +0500

    Transliteration

--- messages/ru-RU/app.php
+++ messages/ru-RU/app.php
@@ -6,24 +6,30 @@
  * Time: 12:31
  */
 return [
+
     // Core
     'FDF Assistant' => 'ФДФ Ассистент',
     'Update' => 'Редактировать',
+
     // DB ladels
     'ID' => 'ID',
     'Name' => 'Название',
     'Slug' => 'Алиас',
+
     // app/modules/admin/main
     'Admin panel' => 'Панель администратора',
+
     // app/modules/admin/club
     'Club' => 'Клуб',
     'Clubs' => 'Клубы',
     'Create Club' => 'Добавить клуб',
     'Update Club: {name}' => 'Редактировать клуб: {name}',
+
     // app/modules/admin/city
     'City' => 'Город',
     'Cities' => 'Города',
     'Create City' => 'Добавить город',
+
     // app/modules/admin/gender
     'Gender' => 'Пол',
     'Genders' => 'Пол',
