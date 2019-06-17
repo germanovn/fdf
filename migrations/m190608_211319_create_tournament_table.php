9c9257e58b103f64f6ff6f3402781820db332794
Nick <germanovbiz@gmail.com>
2019-06-17 18:08:56 +0500

    Change Tournament migration

--- migrations/m190608_211319_create_tournament_table.php
+++ migrations/m190608_211319_create_tournament_table.php
@@ -5,8 +5,6 @@ use yii\db\Migration;
 /**
  * Handles the creation of table `{{%tournament}}`.
  * Has foreign keys to the tables:
- *
- * - `{{%nomination}}`
  */
 class m190608_211319_create_tournament_table extends Migration
 {
@@ -19,25 +17,7 @@ class m190608_211319_create_tournament_table extends Migration
             'id' => $this->primaryKey(),
             'name' => $this->string()->notNull()->unique(),
             'slug' => $this->string()->notNull()->unique(),
-            'nomination_id' => $this->integer()->notNull(),
         ]);
-
-        // creates index for column `nomination_id`
-        $this->createIndex(
-            '{{%idx-tournament-nomination_id}}',
-            '{{%tournament}}',
-            'nomination_id'
-        );
-
-        // add foreign key for table `{{%nomination}}`
-        $this->addForeignKey(
-            '{{%fk-tournament-nomination_id}}',
-            '{{%tournament}}',
-            'nomination_id',
-            '{{%nomination}}',
-            'id',
-            'CASCADE'
-        );
     }
 
     /**
@@ -45,18 +25,6 @@ class m190608_211319_create_tournament_table extends Migration
      */
     public function safeDown()
     {
-        // drops foreign key for table `{{%nomination}}`
-        $this->dropForeignKey(
-            '{{%fk-tournament-nomination_id}}',
-            '{{%tournament}}'
-        );
-
-        // drops index for column `nomination_id`
-        $this->dropIndex(
-            '{{%idx-tournament-nomination_id}}',
-            '{{%tournament}}'
-        );
-
         $this->dropTable('{{%tournament}}');
     }
 }
