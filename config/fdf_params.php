2c73f3fa111395b105d04620003f70edd8532995
Nick <germanovbiz@gmail.com>
2019-06-18 16:24:17 +0500

    Add custom config // Yii::$app->params['fdf_params']

---
+++ config/fdf_params.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by Nick.
+ * User: Nick
+ * Date: 18.06.2019
+ * Time: 16:20
+ */
+
+return [
+    'sex' => [
+        'male'    => 'Male',
+        'female'  => 'Female',
+        'unknown' => 'Unknown',
+    ]
+];
--- config/params.php
+++ config/params.php
@@ -1,7 +1,10 @@
 <?php
 
+$fdf_params = require __DIR__ . '/fdf_params.php';
+
 return [
-    'adminEmail' => 'admin@example.com',
+    'adminEmail'  => 'admin@example.com',
     'senderEmail' => 'noreply@example.com',
-    'senderName' => 'Example.com mailer',
+    'senderName'  => 'Example.com mailer',
+    'fdf_params'  => $fdf_params
 ];
