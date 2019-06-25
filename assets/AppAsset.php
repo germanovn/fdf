f65786f44df3299ce6c916c25f47cbb5cde64052
Nick <germanovbiz@gmail.com>
2019-06-25 16:47:09 +0500

    View TournamentWidget

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -22,6 +22,7 @@ class AppAsset extends AssetBundle
     public $css = [
         'css/site.css',
         'css/widgets-participant-nomination-table.css',
+        'css/tournament-widget.css',
     ];
     public $js = [
     ];
