0c9cfb90c79b54a1324e3936a6d3783130ba8274
Nick <germanovbiz@gmail.com>
2019-09-27 13:35:37 +0500

    Translate

--- messages/ru-RU/app.php
+++ messages/ru-RU/app.php
@@ -7,36 +7,72 @@
  */
 return [
 
-    // Core
-    'FDF Assistant' => 'ФДФ Ассистент',
-    'Update' => 'Редактировать',
-    'Save' => 'Сохранить',
-    'Delete' => 'Удалить',
+    // Core Template
+    'FDF Assistant'                 => 'ФДФ Ассистент',
+    'Admin'                         => 'Администратор',
+    'Update'                        => 'Редактировать',
+    'Save'                          => 'Сохранить',
+    'Delete'                        => 'Удалить',
 
     // DB ladels
-    'ID' => 'ID',
-    'Name' => 'Название',
-    'Slug' => 'Алиас',
+    'ID'                            => 'ID',
+    'Name'                          => 'Название',
+    'Slug'                          => 'Алиас',
 
     // app/modules/admin/main
-    'Admin panel' => 'Панель администратора',
+    'Admin panel'                   => 'Панель администратора',
 
     // app/modules/admin/club
-    'Club' => 'Клуб',
-    'Clubs' => 'Клубы',
-    'Create Club' => 'Добавить клуб',
-    'Update Club: {name}' => 'Редактировать клуб: {name}',
-    'Add at least one city' => 'Добавьте хотя бы один город',
+    'Club'                          => 'Клуб',
+    'Clubs'                         => 'Клубы',
+    'Create Club'                   => 'Добавить клуб',
+    'Update Club: {name}'           => 'Редактировать клуб: {name}',
+    'Add at least one city'         => 'Добавьте хотя бы один город',
 
     // app/modules/admin/city
-    'City' => 'Город',
-    'Cities' => 'Города',
-    'Create City' => 'Добавить город',
-    'Update City: {name}' => 'Редактировать город: {name}',
+    'City'                          => 'Город',
+    'Cities'                        => 'Города',
+    'Create City'                   => 'Добавить город',
+    'Update City: {name}'           => 'Редактировать город: {name}',
 
     // app/modules/admin/gender
-    'Gender' => 'Пол',
-    'Genders' => 'Пол',
-    'Create Gender' => 'Добавить пол',
-    'Update Gender: {name}' => 'Редактировать пол: {name}',
+    'Gender'                        => 'Пол',
+    'Genders'                       => 'Пол',
+    'Create Gender'                 => 'Добавить пол',
+    'Update Gender: {name}'         => 'Редактировать пол: {name}',
+
+    // app/modules/admin/nomination-tournament
+    'Nomination Tournament'         => 'Связь номинация-турнир',
+    'Nomination Tournaments'        => 'Связи номинация-турнир',
+    'Create Nomination Tournament'  => 'Добавить связь номинация-турнир',
+
+    // app/modules/admin/echelons
+    'Echelon'                       => 'Эшелон',
+    'Echelons'                      => 'Эшелоны',
+    'Create Echelon'                => 'Добавить эшелон',
+
+    // app/modules/admin/participants
+    'Participant'                   => 'Участник',
+    'Participants'                  => 'Участники',
+    'Create Participant'            => 'Добавить участника',
+
+    // app/modules/admin/qualifying-scheme
+    'Qualifying Scheme'             => 'Схема проведения отборочного этапа',
+    'Qualifying Schemes'            => 'Схемы проведения отборочного этапа',
+    'Create Qualifying Scheme'      => 'Добавить схему',
+
+    // app/modules/admin/nomination
+    'Nomination'                    => 'Номинация',
+    'Nominations'                   => 'Номинации',
+    'Create Nomination'             => 'Добавить номинацию',
+
+    // app/modules/admin/tournament
+    'Tournament'                    => 'Турнир',
+    'Tournaments'                   => 'Турниры',
+    'Create Tournament'             => 'Добавить турнир',
+
+    // app/modules/admin/participant-nomination
+    'Participant Nomination'        => 'Связь участник-номинация',
+    'Participant Nominations'       => 'Связи участник-номинация',
+    'Create Participant Nomination' => 'Добавить связь участник-номинация',
 ];
